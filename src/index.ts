c543049715cfe5728f22a7da28f967c1b6f3d471
nico_martin <mail@nicomartin.ch>
2020-07-02 00:31:05 +0200

    added env vars

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import 'dotenv/config';
+
 import fs from 'fs';
 import http from 'http';
 import server from './server';
 import { normalizePath } from './helpers';
 
-const port = 1234;
+const PORT = process.env.PORT || 8012;
+const IMAGES_FOLDER = process.env.IMAGES_FOLDER || './images';
 
 const handle = async ({
   request,
@@ -16,16 +19,16 @@ const handle = async ({
   try {
     const url = normalizePath(String(request.url));
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write('<html><body><p>200 ' + url + '</p></body></html>');
+    response.write('200 ' + url);
     response.end();
   } catch (err) {
     response.writeHead(500, { 'Content-Type': 'text/html' });
-    response.write('<html><body><p>500</p></body></html>');
+    response.write('error 500');
     response.end();
   }
 };
 
-server('./images', handle).listen(port, () => {
-  console.log(`Running on Port ${port}`);
-  console.log(`http://localhost:${port}/`);
+server(IMAGES_FOLDER, handle).listen(PORT, () => {
+  console.log(`Running on Port ${PORT}`);
+  console.log(`http://localhost:${PORT}/`);
 });
