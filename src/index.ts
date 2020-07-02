d93a3339e347ed3dbed217da936519b298714e33
nico_martin <mail@nicomartin.ch>
2020-07-02 09:17:01 +0200

    removed images

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import 'dotenv/config';
 
 import fs from 'fs';
 import http from 'http';
-import server from './server';
-import { normalizePath } from './helpers';
+import Jimp from 'jimp';
+import { server, return404, return500 } from './server';
+import fetch from 'node-fetch';
+import { untrailingSlashIt } from './helpers';
 
 const PORT = process.env.PORT || 8012;
 const IMAGES_FOLDER = process.env.IMAGES_FOLDER || './images';
+const IMG_HOST = untrailingSlashIt('https://skateparkguide.ch');
 
 const handle = async ({
   request,
@@ -16,15 +19,43 @@ const handle = async ({
   response: http.ServerResponse;
 }): Promise<void> => {
   // will be called if the static file does not (yet) exists.
+  //
+  // About
+  // Image URL: https://img-host.com/my/path/test.jpg
+  // URL Scheme: https://img-service.com/size-{org|200x200|0x200|200x0}/filter-grayscale-50,blur-50/my/path/test.jpg
   try {
-    const url = normalizePath(String(request.url));
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write('200 ' + url);
-    response.end();
+    if (request.url === '/favicon.ico') {
+      return404(response);
+      return;
+    }
+    const url = IMG_HOST + request.url;
+    const imageRequest = await fetch(url);
+    if (!imageRequest.ok) {
+      return404(response, 'Image not found');
+      return;
+    }
+
+    Jimp.read(await imageRequest.buffer()).then(async image => {
+      // size- has to be calculated: filst crop, then resize
+      image.crop(500, 500, 500, 500);
+      image.resize(100, 100);
+      //image.grayscale();
+      //image.gaussian(15);
+      //image.write(IMAGES_FOLDER + request.url);
+      const mimeType = image.getMIME();
+      const buffer = await image.getBufferAsync(mimeType);
+
+      response.writeHead(imageRequest.status, {
+        ...imageRequest.headers.raw(),
+        'content-length': buffer.length,
+        'content-type': mimeType,
+      });
+      response.write(buffer);
+      response.end();
+    });
   } catch (err) {
-    response.writeHead(500, { 'Content-Type': 'text/html' });
-    response.write('error 500');
-    response.end();
+    return500(response);
+    return;
   }
 };
 
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,10 @@ import http from 'http';
 import staticServer from 'node-static';
 import { untrailingSlashIt } from './helpers';
 
-export default (serveDir: string, handleError: Function): http.Server => {
+export const server = (
+  serveDir: string,
+  handleError: Function
+): http.Server => {
   const file = new staticServer.Server(`./${untrailingSlashIt(serveDir)}`);
   const handleResponse = (
     request: http.IncomingMessage,
@@ -27,4 +30,31 @@ export default (serveDir: string, handleError: Function): http.Server => {
   return http.createServer((request, response) =>
     handleResponse(request, response)
   );
+};
+
+export const return400 = (
+  response: http.ServerResponse,
+  text: string = 'Bad Request'
+) => {
+  response.writeHead(400, { 'Content-Type': 'text/html' });
+  response.write('Error 400: ' + text);
+  response.end();
+};
+
+export const return404 = (
+  response: http.ServerResponse,
+  text: string = 'Not Found'
+) => {
+  response.writeHead(404, { 'Content-Type': 'text/html' });
+  response.write('Error 404: ' + text);
+  response.end();
+};
+
+export const return500 = (
+  response: http.ServerResponse,
+  text: string = 'Internal server error'
+) => {
+  response.writeHead(500, { 'Content-Type': 'text/html' });
+  response.write('Error 500: ' + text);
+  response.end();
 };
